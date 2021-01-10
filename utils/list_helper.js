05725ea2a5daf56e7fee175baa3baad31eab41c4
Saggre <sakri.koskimies@hotmail.com>
2021-01-10 21:56:14 +0200

    Add tests (ex 4.4, 4.5)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
-  return 1;
-};
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => 1;
+
+/**
+ * Get the total number of likes across all blogs
+ * @param blogs
+ */
+const totalLikes = (blogs) => blogs.reduce((carry, blog) => carry + blog.likes, 0);
+
+const favoriteBlog = (blogs) => blogs.reduce((carry, blog) => (carry.likes > blog.likes ? carry : blog));
 
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
