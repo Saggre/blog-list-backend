36da571a2478704732ca7b536f85e5fce5f3fe19
Saggre <sakri.koskimies@hotmail.com>
2021-01-16 20:42:06 +0200

    Set blog required params, fix async beforeAll (ex 4.12)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -11,7 +11,7 @@ blogsRouter.post('/', async (request, response) => {
 
   const blog = new Blog({
     title: body.title,
-    author: body.author,
+    author: body.author || '',
     url: body.url,
     likes: body.likes || 0,
   });
--- tests/api.test.js
+++ tests/api.test.js
@@ -5,21 +5,23 @@ const app = require('../app');
 const api = supertest(app);
 const Blog = require('../models/blog');
 
-const blogs = [{
-  _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0,
-},
-{
-  _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0,
-},
+const blogs = [
+  {
+    title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5,
+  },
 ];
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  await blogs.forEach((b) => new Blog(b).save());
-});
+describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    const blogPromises = blogs.map((blog) => new Blog(blog).save());
+    await Promise.all(blogPromises);
+  });
 
-describe('API tests', () => {
-  test('Blog list application returns the correct amount of blog posts in JSON format', async () => {
+  test('blog list application returns the correct amount of blog posts in JSON format', async () => {
     const response = await api
       .get('/api/blogs')
       .expect(200)
@@ -28,18 +30,10 @@ describe('API tests', () => {
     expect(response.body).toHaveLength(blogs.length);
   });
 
-  test('Blogs contain correct id parameter', async () => {
-    const response = await api.get('/api/blogs');
-
-    response.body.forEach((blog) => {
-      expect(blog.id).toBeDefined();
-    });
-  });
-
-  test('Blog is posted correctly', async () => {
+  test('blog is posted correctly', async () => {
     await api
       .post('/api/blogs')
-      .send(blogs[0])
+      .send({ ...blogs[0] })
       .expect(201)
       .expect('Content-Type', /application\/json/);
 
@@ -50,13 +44,35 @@ describe('API tests', () => {
     expect(titles).toContain(blogs[0].title);
   });
 
-  test('Like count defaults to zero', async () => {
-    const blog = blogs[0];
+  test('blogs contain correct id parameter', async () => {
+    const response = await api.get('/api/blogs');
+
+    response.body.forEach((blog) => {
+      expect(blog.id).toBeDefined();
+    });
+  });
+});
+
+describe('addition of a new blog', () => {
+  test('like count defaults to zero', async () => {
+    const blog = { ...blogs[0] };
     delete blog.likes;
 
     const response = await api.post('/api/blogs').send(blog);
 
     expect(response.body.likes).toEqual(0);
+  });
+
+  test('blog title and url are required properties', async () => {
+    let blog = { ...blogs[0] };
+    delete blog.title;
+
+    await api.post('/api/blogs').send(blog).expect(400);
+
+    blog = { ...blogs[0] };
+    delete blog.url;
+
+    await api.post('/api/blogs').send(blog).expect(400);
   });
 });
 
