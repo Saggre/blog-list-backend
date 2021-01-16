591670fcaff8d5cff075f782f6193533fda4cdf2
Saggre <sakri.koskimies@hotmail.com>
2021-01-16 20:01:37 +0200

    Test post method (ex 4.10)

--- tests/api.test.js
+++ tests/api.test.js
@@ -5,43 +5,48 @@ const app = require('../app');
 const api = supertest(app);
 const Blog = require('../models/blog');
 
-const initialBlogs = [
-  {
-    title: 'Title',
-    author: 'Author Authorina',
-    url: 'http://localhost:8080',
-    likes: 10,
-  },
-  {
-    title: 'Title',
-    author: 'Author Authorina',
-    url: 'http://localhost:8080',
-    likes: 15,
-  },
+const blogs = [{
+  _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0,
+},
+{
+  _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0,
+},
 ];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  await initialBlogs.forEach((b) => new Blog(b).save());
+  await blogs.forEach((b) => new Blog(b).save());
 });
 
-describe('API tests', () => {
-  test('Blog list application returns the correct amount of blog posts in JSON format', async () => {
-    const response = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+test('Blog list application returns the correct amount of blog posts in JSON format', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
 
-    expect(response.body).toHaveLength(initialBlogs.length);
+  expect(response.body).toHaveLength(blogs.length);
+});
+
+test('Blogs contain correct id parameter', async () => {
+  const response = await api.get('/api/blogs');
+
+  response.body.forEach((blog) => {
+    expect(blog.id).toBeDefined();
   });
+});
 
-  test('Blogs contain correct id parameter', async () => {
-    const response = await api.get('/api/blogs');
+test('Blog is posted correctly', async () => {
+  await api
+    .post('/api/blogs')
+    .send(blogs[0])
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
 
-    response.body.forEach((blog) => {
-      expect(blog.id).toBeDefined();
-    });
-  });
+  const response = await api.get('/api/blogs');
+  const titles = response.body.map((r) => r.title);
+
+  expect(response.body).toHaveLength(blogs.length + 1);
+  expect(titles).toContain(blogs[0].title);
 });
 
 afterAll(() => {
